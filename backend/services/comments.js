ca1e8ac525835a8d8ce38d1b466dcf4101128dd7
Robert Abreu <robertabreu@pursuit.org>
2019-04-18 12:40:39 -0400

    comments services and routes created

---
+++ backend/services/comments.js
@@ -0,0 +1,27 @@
+const { db } = require('./db');
+
+const getAllShowComments = (show_id) => {
+    return db.any(`
+    SELECT *
+    FROM comments
+    JOIN shows
+    ON comments.show_id = shows.id
+    WHERE comments.show_id = $[show_id];`, {
+            show_id
+        });
+};
+
+const createComment = (comment_body, user_id, show_id) => {
+    return db.one(`
+    INSERT INTO comments (comment_body, user_id, show_id)
+    VALUES ($[comment_body], $[user_id], $[show_id])
+    RETURNING id;
+    `, { 
+        comment_body, user_id, show_id,
+    });
+};
+
+module.exports = {
+    createComment,
+    getAllShowComments,
+};
