2bafafb320daa5ad2e972cba216364fbd387b58a
Robert Abreu <robertabreu@pursuit.org>
2019-04-20 11:30:50 -0400

    imported new component, added margin top for all future containers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-import { NavBar } from './components';
+import { withStyles } from '@material-ui/core/styles';
+import { Home, NavBar } from './components';
 import { Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Route path='/' component={NavBar}/>
-      </>
-    );
+const styles = {
+  body: {
+    marginTop: '66px',
   }
 }
 
-export default App;
+export default withStyles(styles)(class App extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <>
+        <Route path='/' component={NavBar} />
+        <div className={classes.body}>
+          <Route path='/' exact component={Home} />
+        </div>
+      </>
+    );
+  }
+})
