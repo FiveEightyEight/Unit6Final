bb8aec62291a0c387a20b399629e43986c1f0c31
Robert Abreu <robertabreu@pursuit.org>
2019-04-21 10:25:37 -0400

    updated imports and exports for containers and component directories

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as NavBar } from './NavBar';
 export { default as Home } from './Home';
 export { default as List } from './List';
+export { default as ShowCard } from './ShowCard';
