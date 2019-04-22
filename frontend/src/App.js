f2dab9f34e791507d5618ce68eb10f8715f63398
Robert Abreu <robertabreu@pursuit.org>
2019-04-21 20:16:53 -0400

    exported/imported container to index.js & app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { Home, NavBar } from './components';
-import { Profile, Users, ShowProfile, SwitchUser } from './containers';
+import { Profile, Users, ShowProfile, Shows, SwitchUser } from './containers';
 import { Route, Switch } from 'react-router-dom';
 import { Provider } from './contexts/User';
 import userService from './services/users'
@@ -45,6 +45,7 @@ export default withStyles(styles)(class App extends Component {
                   <Route path='/user/:id' exact component={Profile} />
                   <Route path='/switch/:id' exact component={SwitchUser} />
                   <Route path='/show/:id' exact component={ShowProfile} />
+                  <Route path='/shows' exact component={Shows} />
                 </Provider>
               </Switch>
           }
--- frontend/src/containers/index.js
+++ frontend/src/containers/index.js
@@ -1,4 +1,5 @@
 export { default as Users } from './Users';
 export { default as Profile } from './Profile';
 export { default as ShowProfile } from './ShowProfile';
+export { default as Shows } from './Shows';
 export { default as SwitchUser } from './SwitchUser';
